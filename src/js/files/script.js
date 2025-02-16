79316ffc700776b7bd3dd732f8436c668ab3d61f
hama-dev <hama-dev@mail.ru>
2025-02-16 18:32:49 +0500

    form first commit

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,42 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+
+//*======================= EMAIL JS =============*//
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+   e.preventDefault()
+
+	 // serviceID - templateID - #form - publicKey 
+	 emailjs
+			.sendForm(
+				'service_j02pkik',
+				'template_pnsqi35',
+				'#contact-form',
+				'08BwaBNofwvNRHTUB'
+			)
+			.then(
+				() => {
+					// show message
+					contactMessage.textContent = 'Сообщение отправлено!'
+
+					// Remove message after 5 seconds
+					setTimeout(() => {
+						contactMessage.textContent = ''
+					}, 5000)
+
+					// Clear form
+					contactForm.reset()
+				},
+				() => {
+					// Show error message
+					contactMessage.textContent = 'Ошибка при отправке сообщения!'
+				}
+			)
+}
+
+contactForm.addEventListener('submit', sendEmail)
