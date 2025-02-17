5bb6ca62558ca4b71f900aebf89d52f7a5b4cdb7
hama-dev <hama-dev@mail.ru>
2025-02-17 16:03:44 +0500

    form added ashat's params to js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,38 +6,79 @@ import { flsModules } from "./modules.js";
 
 
 //*======================= EMAIL JS =============*//
+// const contactForm = document.getElementById('contact-form'),
+//       contactMessage = document.getElementById('contact-message')
+
+// const sendEmail = (e) => {
+//    e.preventDefault()
+
+// 	 // serviceID - templateID - #form - publicKey 
+// 	 emailjs
+// 			.sendForm(
+// 				'service_j02pkik',
+// 				'template_pnsqi35',
+// 				'#contact-form',
+// 				'08BwaBNofwvNRHTUB'
+// 			)
+// 			.then(
+// 				() => {
+// 					// show message
+// 					contactMessage.textContent = 'Сообщение отправлено!'
+
+// 					// Remove message after 5 seconds
+// 					setTimeout(() => {
+// 						contactMessage.textContent = ''
+// 					}, 5000)
+
+// 					// Clear form
+// 					contactForm.reset()
+// 				},
+// 				() => {
+// 					// Show error message
+// 					contactMessage.textContent = 'Ошибка при отправке сообщения!'
+// 				}
+// 			)
+// }
+
+
+
+// contactForm.addEventListener('submit', sendEmail)
 const contactForm = document.getElementById('contact-form'),
-      contactMessage = document.getElementById('contact-message')
+	contactMessage = document.getElementById('contact-message')
 
-const sendEmail = (e) => {
-   e.preventDefault()
+const sendEmail = e => {
+	e.preventDefault()
 
-	 // serviceID - templateID - #form - publicKey 
-	 emailjs
-			.sendForm(
-				'service_j02pkik',
-				'template_pnsqi35',
-				'#contact-form',
-				'08BwaBNofwvNRHTUB'
-			)
-			.then(
-				() => {
-					// show message
-					contactMessage.textContent = 'Сообщение отправлено!'
+	// serviceID - templateID - #form - publicKey
+	emailjs
+		.sendForm(
+			'service_9r8leyo',
+			'template_k7nb7ba',
+			'#contact-form',
+			'v3sJvDs6lUmU8Af8a'
+		)
+		.then(
+			() => {
+				// Success message
+				Swal.fire({
+					title: 'Success!',
+					text: 'Message sent successfully!',
+					icon: 'success',
+				})
 
-					// Remove message after 5 seconds
-					setTimeout(() => {
-						contactMessage.textContent = ''
-					}, 5000)
+				// Remove message after five seconds
+				setTimeout(() => {
+					contactMessage.textContent = ''
+				}, 5000)
 
-					// Clear form
-					contactForm.reset()
-				},
-				() => {
-					// Show error message
-					contactMessage.textContent = 'Ошибка при отправке сообщения!'
-				}
-			)
+				// Clear input fields
+				contactForm.reset()
+			},
+			() => {
+				// Show error message
+				contactMessage.textContent = 'Error, message not sent!'
+			}
+		)
 }
 
 contactForm.addEventListener('submit', sendEmail)
